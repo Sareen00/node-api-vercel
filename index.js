172749b3134344c29244792a3aa9152d618b8154
milhauj <julien.milhau-villar@etu.umontpellier.fr>
2023-01-27 09:40:01 +0100

    modification des package et ajout de page htmlX

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // index.js
+const visiteur = require('./Views/visiteur.html')
 const express = require('express')
+const axios = require('axios')
+const nunjucks = require('nunjucks')
+
 
 const app = express()
 const PORT = 4000
@@ -13,8 +17,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    res.send('This is my about route..... ')
+    const html = nunjucks.render(visiteur)
+    res.send(html)
 })
+
+
+
+
+app.get('/connected', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.send('private. user:' + req.user.email)
+})
+
 
 // Export the Express API
 module.exports = app
