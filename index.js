b1f4dd8b4ee102557b7fb6cb2bb34958f3a3fe35
milhauj <julien.milhau-villar@etu.umontpellier.fr>
2023-01-27 12:12:18 +0100

    ajout de la connexion de l'inscription qui accede a la base de donne

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const PORT = 4000
 const axios = require('axios')
 const nunjucks = require('nunjucks')
 
-
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
 const passportJWT = require('passport-jwt')
@@ -50,14 +49,169 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    const html = nunjucks.render('./views/visiteur.html',{})
-    res.send(html)
+    res.send('you ae on the about route')
+
+})
+
+
+//PRIVATE ROUTES
+app.get('/addElement', passport.authenticate('jwt', { session: false }), async function (req, res) {
+    res.send('private. user:' + req.user.email + ' on pourra ajoute un element ou en lodifie un deja cree')
+
+    await axios({
+        method:"post",
+        url: `https://nodemilhauj-3069.restdb.io/rest/utilisateurs`,
+        data:utilisateur,
+        headers:{
+            "x-apikey": "aac82f5b135ec774843b7536945f64f4f57ef",
+        },
+    });
+
+})
+
+app.get('/deleteElement', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.send('private. user:' + req.user.email)
+})
+
+
+
+//PUBLIC ROOTS
+app.get('/element', (req, res) => {
+    res.send('On va selectionne un produit')
+    //CCHERCHE Si element existe
+    //donne element
+
     //res.send('you ae on the about root')
 
 })
 
-app.get('/private', passport.authenticate('jwt', { session: false }), (req, res) => {
-    res.send('private. user:' + req.user.email)
+app.get('/allelements', (req, res) => {
+    res.send('On va selectionne tout les produits produit')
+    //Chercher tout les elements
+})
+
+app.get('/util', async function (req, res){
+    const response1 = await axios({
+        method:"get",
+        url: `https://nodemilhauj-3069.restdb.io/rest/utilisateurs`,
+        headers:{
+            "x-apikey": "aac82f5b135ec774843b7536945f64f4f57ef",
+        },
+    });
+
+    //console.log(response1.data)
+
+    var users =[]
+    for (const response1Element of response1.data) {
+        console.log(response1Element.email)
+        tablogin.push({email: response1Element.email, password: response1Element.password})
+    }
+    console.log(tablogin)
+
+
+    const user = users.find(user => user.email === email)
+    if (!user){
+        res.status(401).json({ error: 'Utilisateur inconnu il faut être inscrit.' })
+        return
+    }else if(user.password !== password){
+        res.status(401).json({ error: 'Email / password do not match.' })
+        return
+    }
+
+
+    /*console.log(response1.data[0].email)
+    console.log(response1.data[1].email)
+    console.log(response1.data[2].email)*/
+    
+
+    //console.log(response1.data)
+    res.send('bravo')
+})
+
+
+
+//POST ROUTES
+app.post('/inscription', async function (req, res){
+    const email = req.body.email
+    const pseudo = req.body.pseudo
+    const password = req.body.password
+    const confirmPassword = req.body.confirmPassword
+
+    if (!email || !password || !pseudo || !confirmPassword) {
+        res.status(401).json({ error: 'Email, password,pseudo or password confirmation was not provided.' })
+        return
+    }else if (password !== confirmPassword){
+        res.status(401).json({ error: 'The password and the confirmation password are different.' })
+        return
+    }
+
+    const utilisateur = {
+        email: email,
+        pseudo: pseudo,
+        password: password
+    };
+
+    await axios({
+        method:"post",
+        url: `https://nodemilhauj-3069.restdb.io/rest/utilisateurs`,
+        data:utilisateur,
+        headers:{
+            "x-apikey": "aac82f5b135ec774843b7536945f64f4f57ef",
+        },
+    });
+
+    //const afficheInfo = await console.log(email,pseudo,password,confirmPassword)
+    res.send('Bravo vous êtes inscrit. Connectez vous ici (email et password> /connexion')
+
+})
+
+app.post('/connexion', async function (req, res) {
+
+    console.log(req.body.email)
+    console.log(req.body.password)
+
+    const email = req.body.email
+    const password = req.body.password
+
+    if (!email || !password) {
+        res.status(401).json({ error: 'Email or password was not provided.' })
+        return
+    }
+
+    const response1 = await axios({
+        method:"get",
+        url: `https://nodemilhauj-3069.restdb.io/rest/utilisateurs`,
+        headers:{
+            "x-apikey": "aac82f5b135ec774843b7536945f64f4f57ef",
+        },
+    });
+
+    //console.log(response1.data)
+
+    var users =[]
+    for (const response1Element of response1.data) {
+        console.log(response1Element.email)
+        users.push({email: response1Element.email, password: response1Element.password})
+    }
+    console.log(users)
+
+
+    const user = users.find(user => user.email === email)
+    if (!user){
+        res.status(401).json({ error: 'Utilisateur inconnu il faut être inscrit.' })
+        return
+    }else if(user.password !== password){
+        res.status(401).json({ error: 'Email / password do not match.' })
+        return
+    }
+
+    if (!user || user.password !== password) {
+        res.status(401).json({ error: 'Email / password do not match.' })
+        return
+    }
+
+    const userJwt = jwt.sign({ email: user.email }, secret)
+    res.json({ jwt: userJwt, ajout: '/addelement',suppression: '/deleteelement'})
 })
 
 
